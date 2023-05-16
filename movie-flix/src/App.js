dd7a51dd895692ac223cdaf1552ba2eadbe0c4f0
Khushi Gandhi <khushigandhi683@gmail.com>
2023-05-16 15:44:05 +0530

    Modified files

--- movie-flix/src/App.js
+++ movie-flix/src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
+import React from "react";
+import {useEffect} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+const API_URL='https://www.omdbapi.com?apikey=7bbdd6f2'
+const App=()=>{
+    return(
+        <h1>Hello</h1>
+    );
 }
-
 export default App;
